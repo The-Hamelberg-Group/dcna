9566f0462c365c898403b13e0ed2e7e92851836a
Xin-Qiu Yao <xinqiu.yao@gmail.com>
2021-07-14 12:12:37 -0400

    OTHER: slight update of documents

--- community/run_community_analysis.r
+++ community/run_community_analysis.r
@@ -1,5 +1,4 @@
 
-############ PARAMETERS YOU CAN ADJUST ################################
 # Filename of the first contact statistics file
 contfile1 = '../example/free'
 
@@ -16,11 +15,10 @@ qcutoff = 0.9
 # (0.1 means if contact frequency difference between the two states is less than 10%, ignore them)
 fcutoff = 0.1
 
-# number of communities to show; If set `NULL`, use the automatically determined number.
+# number of communities to show; If set `NULL`, use the automatically determined number (which maximizes the modularity).
 num.comm = NULL
 
 
-############# DON'T CHANGE FOLLOWING LINES (UNLESS YOU KNOW WHAT IT MEANS) ##########
 library(bio3d)
 library(igraph)
 source('funs.R')
@@ -29,6 +27,9 @@ freqs <- lapply(list(f1=contfile1, f2=contfile2), read.table)
 pdb <- read.pdb(pdbfile)
 nets <- buildnet(freqs, pdb, qcut=qcutoff, fcut=fcutoff, ncom=num.comm)
 plot_community(nets[[1,2]], pdb=pdb, fcut=fcutoff)
+
+
+#### Uncomment some of the following lines for specified output ####
 
 ## Save manually adjusted layout and replot
 # layout <- tk_coords(1)  ## The number "1" is the number shown on the top of the plot window
@@ -40,5 +41,4 @@ plot_community(nets[[1,2]], pdb=pdb, fcut=fcutoff)
 ## check modularity scores for alternative "num.comm" values
 # rem <- community.tree(nets[[1,2]])
 # plot(rem$num.of.comm, rem$modularity, typ='b', cex=0.6, xlab='Number of Communities', ylab='Modularity')
-
 
